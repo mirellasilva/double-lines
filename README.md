d0017ef113cf46e04ae8399ca98ca748c445598e
Mirella Silva <mirella@goldair.local>
2024-01-25 14:36:15 -0300

    first working version

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# double-lines
-Double Lines Challenge
+# Double Lines Challenge
+
+Algorithm that draws two lines side by side without overlapping. It must handle an arbitrary amount of points where each 3 consecutive points are forming a 90 degrees angle.
+
+This code was developed in the context of a challenge. Read more about it [here](https://github.com/vjeux/weekly-challenge-2-double-lines).
+
+
+## Requirements
+
+* Python 
+* matplotlib
+
+## How to Run
+
+You can both use sys.argv to enter with the points, separated by space, such as "x1 y1 x2 y2 ... xn yn", or follow instructions for io input.
+
+Sample inputs: 
+
+```
+python3 lines2.py 5 13 5 9 8 9 8 6 2 6 2 9
+python3 lines2.py 10 13 7 13 7 10 4 10 4 7 7 7 7 4 13 4 13 1 16 1 16 12 13 12 13 9 11 9 11 2
+```
+
+
+
+
+
+
+
+
---
+++ dlines.py
@@ -0,0 +1,156 @@
+import sys
+import matplotlib.pyplot as plt
+
+DISTANCE = 0.5
+# python3 lines2.py 5 13 5 9 8 9 8 6 2 6 2 9
+# python3 lines2.py 10 13 7 13 7 10 4 10 4 7 7 7 7 4 13 4 13 1 16 1 16 12 13 12 13 9 11 9 11 2
+
+def draw_lines(ax, coordinates):
+
+    if len(coordinates) < 3: 
+        return
+
+    plot_coordinates(ax, coordinates, "yellow")
+
+    red_x, red_y = blue_x, blue_y = coordinates[0]
+    is_horizontal_line = does_it_repeat_y(coordinates, 0)
+
+    # first point is different
+    if is_horizontal_line:
+        # red_x is done
+        red_y = red_y - DISTANCE
+        blue_y = blue_y + DISTANCE
+    else:
+        # red_y is done
+        red_x = red_x + DISTANCE
+        blue_x = blue_x - DISTANCE
+
+    red_coordinates = [(red_x, red_y)]
+    blue_coordinates = [(blue_x, blue_y)]
+    red_prev_x, red_prev_y = red_x, red_y
+    blue_prev_x, blue_prev_y = blue_x, blue_y
+    print(" prev red: ", red_prev_x, red_prev_y, " prev blue: ", blue_prev_x, blue_prev_y)
+    for i in range(1, len(coordinates) - 1):
+        red_next_x, red_next_y = get_next_red_point(red_prev_x, red_prev_y, coordinates, i, is_horizontal_line)
+        blue_next_x, blue_next_y = get_next_blue_point(blue_prev_x, blue_prev_y, coordinates, i, is_horizontal_line)
+        
+        red_coordinates.append((red_next_x, red_next_y))
+        blue_coordinates.append((blue_next_x, blue_next_y))
+        #ax.plot([red_prev_x, red_prev_y], [red_next_x, red_next_y], color="red")
+        #ax.plot([blue_prev_x, blue_prev_y], [blue_next_x, blue_next_y], color="blue")
+
+        is_horizontal_line = not is_horizontal_line
+        red_prev_x, red_prev_y = red_next_x, red_next_y
+        blue_prev_x, blue_prev_y = blue_next_x, blue_next_y
+        print(" prev red: ", red_prev_x, red_prev_y, " prev blue: ", blue_prev_x, blue_prev_y)
+
+    
+    red_x, red_y = blue_x, blue_y = coordinates[len(coordinates) - 1]
+
+    # last point is different
+    if is_horizontal_line:
+        # red_x is done
+        red_y = red_y - DISTANCE
+        blue_y = blue_y + DISTANCE
+    else:
+        # red_y is done
+        red_x = red_x + DISTANCE
+        blue_x = blue_x - DISTANCE
+
+    red_coordinates.append((red_x, red_y))
+    blue_coordinates.append((blue_x, blue_y))
+    
+    plot_coordinates(ax, red_coordinates, "red")
+    plot_coordinates(ax, blue_coordinates, "blue")
+    #ax.plot([red_prev_x, red_prev_y], [last_x, last_y], color="red")
+    #ax.plot([blue_prev_x, blue_prev_y], [last_x, last_y], color="blue") 
+
+def get_next_red_point(prev_x, prev_y, coordinates, p, is_horizontal_line):
+    x1, y1 = coordinates[p] # B
+    x2, y2 = coordinates[p + 1] # C
+    next_x, next_y = prev_x, prev_y
+    if is_horizontal_line:
+        if y1 < y2:
+            next_x = x1 - DISTANCE
+        else: 
+            next_x = x1 + DISTANCE
+    else: 
+        if x1 < x2:
+            next_y = y1 + DISTANCE
+        else: 
+            next_y = y1 - DISTANCE
+    return (next_x, next_y)
+
+
+def get_next_blue_point(prev_x, prev_y, coordinates, p, is_horizontal_line):
+    x1, y1 = coordinates[p] # B
+    x2, y2 = coordinates[p + 1] # C
+    next_x, next_y = prev_x, prev_y
+    if is_horizontal_line:
+        if y1 < y2:
+            next_x = x1 + DISTANCE
+        else: 
+            next_x = x1 - DISTANCE
+    else: 
+        if x1 < x2:
+            next_y = y1 - DISTANCE
+        else: 
+            next_y = y1 + DISTANCE
+    return (next_x, next_y)
+
+def does_it_repeat_y(coordinates, p):
+    x1, y1 = coordinates[p]
+    x2, y2 = coordinates[p + 1]
+    if x1 == x2:
+        return False
+    elif y1 == y2:
+        return True
+
+def plot_coordinates(ax, coordinates, line_color):
+    # plot regular line
+    for i in range(len(coordinates) - 1):
+        x1, y1 = coordinates[i]
+        x2, y2 = coordinates[i + 1]
+        ax.plot([x1, x2], [y1, y2], color=line_color)
+
+# Check if command line arguments are provided
+if len(sys.argv) > 1:
+    # Extract coordinates from command line arguments
+    coordinates = [(float(sys.argv[i]), float(sys.argv[i + 1])) for i in range(1, len(sys.argv), 2)]
+else:
+    # Read coordinates from user input
+    n = int(input("Enter the number of coordinates: "))
+    coordinates = []
+    for i in range(n):
+        input_str = input(f"Enter x{i+1} y{i+1} (separated by space): ")
+        x, y = map(float, input_str.split())
+        coordinates.append((x, y))
+
+# Create a figure and axis
+fig, ax = plt.subplots()
+
+# Call the function to draw lines connecting all points
+draw_lines(ax, coordinates)
+
+# Set axis limits
+min_x = min(x for x, y in coordinates) - 3
+max_x = max(x for x, y in coordinates) + 3
+min_y = min(y for x, y in coordinates) - 3
+max_y = max(y for x, y in coordinates) + 3
+ax.set_xlim(min_x, max_x)
+ax.set_ylim(min_y, max_y)
+
+# Set axis labels
+ax.set_xlabel('X-axis')
+ax.set_ylabel('Y-axis')
+
+# Set plot title
+ax.set_title('Lines Connecting All Points')
+
+# Display legend
+ax.legend()
+
+# Show the plot
+plt.show()
+
+
